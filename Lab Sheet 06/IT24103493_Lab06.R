21b43e11c12b077b50527bf0d4219353a2de221f
IT24103493 <IT24103493@my.sliit.lk>
2025-09-15 22:47:30 +0530

    Lab_06

---
+++ Lab Sheet 06/IT24103493_Lab06.R
@@ -0,0 +1,29 @@
+setwd("C:\\Users\\user\\Desktop\\IT24103493")
+
+# Exercise
+
+# Question 1
+# Part 1
+
+# Binomial Distribution
+# random variable x has binomial distribution with n=50 and p=0.85
+
+
+# Part 2
+
+pbinom(46,50,0.85,lower.tail = FALSE)
+
+
+# Question 2
+# Part 1
+
+# X= Number of calls received in one hour
+
+# Part 2
+
+# Poisson Distribution
+# random variable x has poisson distribution with lambda=12
+
+# Part 3
+
+dpois(15,12)
